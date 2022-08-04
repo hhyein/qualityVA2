e50e18cd1e461d474b8f05bf507c0fee6d82de16
Jisue <49144667+Jisue@users.noreply.github.com>
2022-08-04 22:11:55 +0900

    add action dropDown

--- templates/src/components/modules/combination/index.js
+++ templates/src/components/modules/combination/index.js
@@ -16,6 +16,39 @@ export default function Combination() {
   } = useFileData()
   const { combinationData } = combinationTableData
 
+  const [lengthList, setLengthList] = React.useState();
+  const [lengthValues, setLengthValues] = React.useState();
+  const [actionList, setActionList] = React.useState();
+  const [actionValues, setActionValues] = React.useState();
+  const [checkedList, setCheckList] = React.useState();
+
+  React.useEffect(() => {
+    setLengthList(Array.from({ length: 11 }, (_, i) => ({ label: i, value: i })));
+    setCheckList(Array.from({ length: 15 }, (_, i) => i));
+    const imgList = ['', 'em', 'knn', 'lof', 'max', 'med', 'men', 'min', 'mod', 'rem', 'log', 'mbs', 'mm', 'rob', 'sqt', 'std'].reduce(
+      (acc, cur) => ({
+        ...acc,
+        [cur]: (
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            {[cur].map(imgName => (
+              <img
+                src={require(`../../icons/${imgName}.png`)}
+                alt={''}
+                style={{ height: '25px', width: '25px' }}
+              />
+            ))}
+          </div>
+        ),
+      }),
+    )
+    setActionList(Object.values(imgList).map((chart, idx) => (
+      {
+        label: <>{chart}</>,
+        value: idx
+      }
+    )))
+  }, [])
+
   const data = useMemo(() => {
     if (!combinationData) {
       return []
@@ -71,35 +104,63 @@ export default function Combination() {
     [setCombinationTableSortingInfo]
   )
 
-  const [selectList, setSelectList] = React.useState([
-    {
-      label: 'item0',
-      value: 0
-    },
-    {
-      label: 'item1',
-      value: 1
+  const handleChange = (key, value) => {
+    if (key === 'maxLength') {
+      setLengthValues(value);
+    } else {
+      setActionValues(value);
     }
-  ])
+  }
+
+  const handleCheckBox = (checkId) => {
+    if (!checkedList.includes(checkId)) {
+      setCheckList([...checkedList, checkId]);
+    } else {
+      setCheckList(checkedList.filter((checkedId) => checkedId !== checkId));
+    }
+  };
+
 
   return (
     <Box title="combination">
       <div style={{ height: '260px' }}>
         {!isEmptyData({ combinationData }) && data.length > 0 && (
           <>
-            <div style={{ display: 'flex', marginBottom: '10px' }}>
+            <div style={{
+              display: 'flex',
+              marginBottom: '5px'
+            }}>
+              <div style={{
+                width: '46%',
+                margin: '0 2%'
+              }}>
+                <Title title="max length" />
                 <Select
-                  options={selectList}
+                  options={lengthList}
                   placeholder={<div>select</div>}
+                  onChange={v => {
+                    handleChange('maxLength', v)
+                  }}
                 />
-                <Select
-                  options={selectList}
-                  placeholder={<div>select</div>}
-                />
-                <Select
-                  options={selectList}
-                  placeholder={<div>select</div>}
-                />
+              </div>
+              <div style={{
+                width: '46%',
+                margin: '0 2%'
+              }}>
+                <Title title="action" />
+                <div class="dropdown">
+                  <button class="dropbtn">select</button>
+                  <div class="dropdown-content">
+
+                    {actionList && actionList.map((item, idx) => (
+                      <div style={{ display: 'flex', height: '25px' }} >
+                        {item.label}
+                        <input style={{ width: '15px' }} type="checkbox" id="scales" name="scales" checked={checkedList.includes(idx)} onChange={() => handleCheckBox(idx)} />
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
             </div>
             <div style={{ overflow: 'auto', height: '100%' }}>
               <CombinationTable
@@ -139,6 +200,45 @@ export default function Combination() {
           </>
         )}
       </div>
+      <style jsx>{`
+      .dropbtn {
+        background-color: #eee;
+        width: 142px;
+        height: 38px;
+        color: gray;
+        border: none;
+      }
+      .dropdown {
+        position: relative;
+        display: inline-block;
+      }
+      .dropdown-content {
+        overflow: auto;
+        display: none;
+        position: absolute;
+        background-color: #f1f1f1;
+        width: 142px;
+        height: 300px;
+        box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+        z-index: 1;
+      }
+      .dropdown-content div {
+        color: black;
+        // height: 38px;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+      }
+      .dropdown-content div:hover {
+        background-color: #ddd;
+      }
+      .dropdown:hover .dropdown-content {
+        display: block;
+      }
+      .dropdown:hover .dropbtn {
+        background-color: #ddd;
+      }
+        `}</style>
     </Box>
   )
 }
