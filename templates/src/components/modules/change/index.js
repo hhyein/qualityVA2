89b43e9cec9beb0643fda4442035a4ce227df475
Jisue <49144667+Jisue@users.noreply.github.com>
2022-09-26 00:32:54 +0900

    add change title

--- templates/src/components/modules/change/index.js
+++ templates/src/components/modules/change/index.js
@@ -3,6 +3,7 @@ import { Box } from '../../Box'
 import { useFileData } from '../../../contexts/FileDataContext'
 import BarChart from '../../charts/BarChart'
 import DensityChart from '../../charts/DensityChart'
+import Title from '../../Title'
 
 export default function Change() {
   const {
@@ -15,8 +16,18 @@ export default function Change() {
       {!isEmptyData({
         settingValues
       }) && settingValues.model && <>
-        <BarChart />
-        <DensityChart />
+        <div style={{ padding: '0 10px', fontSize: '15px' }}>
+          <Title title="count" />
+        </div>
+        <div style={{ height: '170px' }}>
+          <BarChart />
+        </div>
+        <div style={{ padding: '5px 10px 0 10px', fontSize: '15px' }}>
+          <Title title="distort" />
+        </div>
+        <div style={{ position: 'relative', bottom: '20px' }}>
+          <DensityChart />
+        </div>
       </>}
     </Box>
   )
