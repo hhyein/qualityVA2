5094e649938b3954c09f07adb68ca28a0d3713c0
Seongbum Seo <seo@seongbum.com>
2022-12-03 14:37:40 +0900

    Remove unused message

--- templates/src/contexts/FileDataContext.js
+++ templates/src/contexts/FileDataContext.js
@@ -147,7 +147,6 @@ export const FileDataProvider = ({ children }) => {
 
   useEffect(() => {
     updateQualityImpactData()
-    console.log("SDF")
   }, [])
 
   const updateRecommendData = async () => {
